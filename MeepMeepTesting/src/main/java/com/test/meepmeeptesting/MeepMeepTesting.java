5f86c42f544ac62a6d39ca609a191eb471f23b30
Mira <dinu.gabriel2007@gmail.com>
2023-03-08 22:53:31 +0200

    added opencv

--- MeepMeepTesting/src/main/java/com/test/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/test/meepmeeptesting/MeepMeepTesting.java
@@ -32,21 +32,18 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setDimensions(13.7,13.7)
                 .setConstraints(50, 50  , Math.toRadians(270), Math.toRadians(270), 13.7)
-                .followTrajectorySequence(drive -> {
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(34 , -63 , Math.toRadians(90)))
+                        .waitSeconds(1)
 
-                    return drive.trajectorySequenceBuilder(new Pose2d(34 , -63 , Math.toRadians(90)))
-                            .waitSeconds(1)
+                        .lineTo(new Vector2d(34 + 1,-63+30))
+                        .splineToSplineHeading(new Pose2d(34 + 2,-63 + 53,Math.toRadians(90-40)),Math.toRadians(40))
 
-                            .lineTo(new Vector2d(34 + 1,-63+30))
-                            .splineToSplineHeading(new Pose2d(34 + 2,-63 + 53,Math.toRadians(90-40)),Math.toRadians(40))
+                        .splineToSplineHeading(new Pose2d(34-10,-63+50,Math.toRadians(90 - 90)),Math.toRadians(180))
+                        //.lineToLinearHeading(new Pose2d(34+3,-63+50,Math.toRadians(180)))
+                        //.waitSeconds(10)
+                        .splineTo(new Vector2d(34-24,-63+50),Math.toRadians(180))
 
-                            .splineToSplineHeading(new Pose2d(34-10,-63+50,Math.toRadians(90 - 90)),Math.toRadians(180))
-                            //.lineToLinearHeading(new Pose2d(34+3,-63+50,Math.toRadians(180)))
-                            //.waitSeconds(10)
-                            .splineTo(new Vector2d(34-24,-63+50),Math.toRadians(180))
-
-                            .build();
-                });
+                        .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(false)
